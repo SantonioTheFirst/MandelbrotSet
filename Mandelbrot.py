d33e8a770630b86205f164bedca60906d253e5f1
SantonioTheFirst <santoniothefirst123@gmail.com>
2022-05-20 21:21:11 +0300

    Animations

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageOps
+import imageio
 
-
-maxiter = 10000
+start_iter = 17
+maxiter = 200
 border = 4
 
 x, y = -0.793197078177363, 0.16093721735804
@@ -14,54 +15,60 @@ pmax = x + 0.01
 qmin = y - 0.01
 qmax = y + 0.01
 
-# pmin = -2
-# pmax = 2
+# pmin = -2.5
+# pmax = 1.5
 # qmin = -2
 # qmax = 2
 
-ppoints = 2000
-qpoints = 2000
+ppoints = 1000
+qpoints = 1000
 
 
-def Mandelbrot(maxiter, border, pmin, pmax, qmin, qmax, ppoints, qpoints):
+def Mandelbrot_step(step, z, c, border, image):
+    z = z ** 2 + c
+    mask = ((np.abs(z) > border) & (image == 0))
+    image[mask] = step
+    z[mask] = np.nan
+    return z, image
+
+
+def Mandelbrot(maxiter, border, pmin, pmax, qmin, qmax, ppoints, qpoints, return_points=False, transpose=False):
     image = np.zeros((ppoints, qpoints))
     p, q = np.mgrid[pmin:pmax:(ppoints * 1j), qmin:qmax:(qpoints * 1j)]
     c = p + 1j * q
     z = np.zeros(c.shape, dtype=np.complex128)
     for i in range(maxiter):
-        z = z ** 2 + c
-        mask = ((np.abs(z) > border) & (image == 0))
-        image[mask] = i
-        z[mask] = np.nan
-    return image.T
+        z, image = Mandelbrot_step(i, z, c, border, image)
+    if transpose:
+        image = image.T
+        if return_points:
+            c = c.T
+            z = z.T
+    if not return_points:
+        return image
+    else:
+        return (image, c, z)
+
+
+def make_pictures(start_iter, maxiter, border, pmin, pmax, qmin, qmax, ppoints, qpoints):
+    image, c, z = Mandelbrot(start_iter, border, pmin, pmax, qmin, qmax, ppoints, qpoints, return_points=True, transpose=True)
+    for i in range(start_iter, maxiter):
+        z, image = Mandelbrot_step(i, z, c, border, image)
+        img = (image * 255. / np.max(image)).astype('uint8')
+        img = Image.fromarray(img, 'L')
+        img = ImageOps.colorize(img, black='black', white='white', mid='green', blackpoint=5, whitepoint=255)
+        img.save(f'result_{i}.png')
+
+    
+def make_gif(start_iter, maxiter):
+    images = []
+    for i in range(start_iter, maxiter - 1):
+        im = f'result_{i}.png'
+        images.append(imageio.imread(im))
+    imageio.mimsave('animation.gif', images)
 
 
 start = time()
-image = Mandelbrot(maxiter, border, pmin, pmax, qmin, qmax, ppoints, qpoints)
-image = (image * 255. / np.max(image)).astype('uint8')
-image = np.dstack((image, image, image))
-
-img = Image.fromarray(image, mode='RGB')
-
-r, g, b = img.split()
-
-rr = np.random.uniform(0.5, 3.5)
-rg = np.random.uniform(0.5, 3.5)
-rb = np.random.uniform(0.5, 3.5)
-
-print(rr, rg, rb)
-
-r = r.point(lambda i: i * rr)
-g = g.point(lambda i: i * rg)
-b = b.point(lambda i: i * rb)
-
-result = Image.merge('RGB', (r, g, b))
-
-filter = ImageEnhance.Brightness(result)
-
-result = filter.enhance(maxiter / 1200.)
-
+make_pictures(start_iter, maxiter, border, pmin, pmax, qmin, qmax, ppoints, qpoints)
+make_gif(start_iter, maxiter)
 print(time() - start)
-
-result.save('result.png')
-result.show()
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Python implementation of Mandelbrot set visualization.
 
 ![Mandelbrot set](result.png "Zoomed in Mandelbrot set")
+![Mandelbrot set](animation.gif "Zoomed in Mandelbrot set")
+![Mandelbrot set](animation_2.gif "Mandelbrot set")
